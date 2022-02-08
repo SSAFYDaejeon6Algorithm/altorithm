25824a7774a61a84e6ec7e8f42ade6a01932d3cc
한윤석 <hys6078@naver.com>
2022-02-08 21:50:28 +0900

    백준 10870 피보나치 수 5

--- 한윤석/0209/b10870.java
+++ 한윤석/0209/b10870.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//백준 10870 피보나치 수 5
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class Main {
 		int N = sc.nextInt();
 		arr[0]=0; arr[1]=1;
 		
+		//i번째 피보나치 수는 i-1번 + i-2번
 		if(N >= 2)
 		for(int  i=2; i<=N; i++) 
 			arr[i] = arr[i-1]+arr[i-2];
