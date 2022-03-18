d085d55d879ee26653fc63a9f7131c29e4348167
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-03-18 22:05:37 +0900

    0318 / 1347-미로 만들기, 2448-별 찍기 11
    1347 - 미로 만들기
    적당이 큰 미로를 만들어놓고, 입력에 따라 구멍 뚫기
    이후 가장 왼쪽 위, 오른쪽 아래 좌표를 구해서 미로 출력
    
    2448 - 별 찍기 11
    나 틀림... 왜 틀렸는지 같이 디버깅좀 ㅎㅎ
    출력은 잘되는듯?

---
+++ 우상욱/p_2448.java
@@ -0,0 +1,41 @@
+package bj;
+
+import java.util.*;
+import java.io.*;
+
+public class p_2448 {
+
+	static char pic[][];
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		pic = new char[N][2* N - 1];
+
+		star(N, 0, 0);
+
+		// print result
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 2 * N - 1; j++)
+				sb.append(pic[i][j]);
+			sb.append('\n');
+		}
+		System.out.print(sb);
+	}
+
+	static void star(int n, int x, int y) {
+		if (n == 3) {
+			// 반복문은 거부한다
+			pic[x][y + 2] = '*';
+			pic[x + 1][y + 1] = pic[x + 1][y + 3] = '*';
+			pic[x + 2][y] = pic[x + 2][y + 1] = pic[x + 2][y + 2] = pic[x + 2][y + 3] = pic[x + 2][y + 4] = '*';
+			return;
+		}
+		
+		star(n / 2, x, y + n/2);			// top
+		star(n / 2, x + n/2, y); 			// left
+		star(n / 2, x + n/2, y + n);		// right
+	}
+}
