b54edb9347aeae825757c7df13d7243d0d479058
only444me <only444me@naver.com>
2022-06-21 21:28:58 +0900

    김채윤_프로그래머스_이진변환반복

---
+++ 김채윤/0621/Solution_p70129_이진변환반복하기.java
@@ -0,0 +1,28 @@
+package vac;
+
+public class Solution_p70129_이진변환반복하기 {
+
+	public int[] solution(String s) {
+        int [] answer =new int [2];
+        int changecnt=0; //몇 번의 이진변환을 거쳐야하는지
+        int zerocnt=0; //제거할 0의 개수
+        
+        while(true){
+            int onecnt=0;
+            if(s.equals("1"))
+            	break;
+            for(int i=0; i<s.length(); ++i){
+                if(s.charAt(i)=='0')
+                	zerocnt++;
+                else 
+                	onecnt++;
+            }
+            s=Integer.toBinaryString(onecnt);
+            changecnt++;
+        }
+        answer[0]=changecnt;
+        answer[1]=zerocnt;
+
+        return answer;
+    }
+}
